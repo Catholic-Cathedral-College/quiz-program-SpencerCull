62bde7f8236933c31b2aff49339b15ca00d280f8
SpencerC4 <>
2022-09-01 23:54:43 +0000

    connecting github

---
+++ main.py
@@ -0,0 +1,74 @@
+score = 0
+print("Welcome to the Call of Duty Quiz")
+starting_question = input("Are your ready! Type Yes to continiue and No to cancel")
+if starting_question == "Yes": 
+
+  print("Question 1 - When was the first Call Of Duty game released?")
+question_one = input("A - 2010   B - 2006   C - 2003")
+if question_one == "2003" or question_one.lower() == "c":
+    score += 1
+    print("Correct")
+    print("Your score is",score)
+else:
+    print("Incorrect")
+    print("Your score is",score)
+
+print("Question 2 - What is Rebirth Island in Call of Duty Warzone based on?")
+question_one = input("A - Alacatraz   B - White House   C - Pyrimid of Giza")
+if question_one == "Alcatraz" or question_one.lower() == "a":
+    score += 1
+    print("Correct")
+    print("Your score is",score)
+else:
+    print("Incorrect")
+    print("Your score is",score)
+
+print("Question 3 - What comapny owns Call of Duty?")
+question_one = input("A - Epic Games   B - Activision   C - Bungie")
+if question_one == "Activision" or question_one.lower() == "b":
+    score += 1
+    print("Correct")
+    print("Your score is",score)
+else:
+    print("Incorrect")
+    print("Your score is",score)
+
+print("Question 4 - What is Captain Price's full name?")
+question_one = input("A - John   B - Sam   C - James")
+if question_one == "John" or question_one.lower() == "a":
+    score += 1
+    print("Correct")
+    print("Your score is",score)
+else:
+    print("Incorrect")
+    print("Your score is",score)
+
+print("Question 5 - What is the goal in Search and Destroy?")
+question_one = input("A - Detonate Bomb Sites   B - Every Player For Themselves   C - Capture The Flag")
+if question_one == "Detonate Bomb Sites" or question_one.lower() == "a":
+    score += 1
+    print("Correct")
+    print("Your score is",score)
+else:
+    print("Incorrect")
+    print("Your score is",score)
+
+print("Question 6  - True or False Simon 'Ghost' Riley dies?")
+question_one = input("A - True   B - False")
+if question_one == "True" or question_one.lower() == "a":
+    score += 1
+    print("Correct")
+    print("Your score is",score)
+else:
+    print("Incorrect")
+    print("Your score is",score)
+
+print("Question 7 - Who kills Ghost and Roach?")
+question_one = input("A - Makarov   B - Shepard   C - Perseus")
+if question_one == "Shepard" or question_one.lower() == "b":
+    score += 1
+    print("Correct")
+    print("Your score is",score)
+else:
+    print("Incorrect")
+    print("Your score is",score)
